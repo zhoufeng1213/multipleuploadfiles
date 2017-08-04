daf56b2ee07f383356e5e7cacc20c81e31bd772e
zhoufeng <zhoufeng@btzh.cn>
2017-08-04 11:23:44 +0800

    origin master

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # 多文件上传
+包含一张张图片循环上传和多张一起上传
+集成了retrofit2.0和php上传，其中包含了recycleview横向显示图片、图片压缩、显示多张附件放大缩小
 
 ### 项目截图:
 
-<img src="https://github.com/zhoufeng1213/multipleuploadfiles/111.png" width="320px" style="display:inline;">
+<img src="https://github.com/zhoufeng1213/multipleuploadfiles/blob/master/111.png" width="320px" style="display:inline;">
     
-<img src="https://github.com/zhoufeng1213/multipleuploadfiles/222.png" width="320px" style="display:inline;">
+<img src="https://github.com/zhoufeng1213/multipleuploadfiles/blob/master/222.png" width="320px" style="display:inline;">
---
+++ php/uploadMultipleFiles.php
@@ -0,0 +1,58 @@
+﻿<?php
+
+
+
+//保存路径
+$uploaddir = $_REQUEST['path'];
+
+ //操作类型0：上传   1：删除
+$type = $_REQUEST['type'];
+
+$uploaddir = $uploaddir."/";
+if(!file_exists($uploaddir))
+{
+	//mkdir($uploaddir);
+	mkdir($uploaddir,0777,true);
+}
+if($type==0){
+	if(isset($_REQUEST['fileCount'])){
+		$isSave = true;
+		$filecount = $_REQUEST['fileCount'];
+		for($i = 0;$i < $filecount;$i++){
+			//上传图片名称
+			//$filename = $_REQUEST['filename'.$i];
+			$filename = $_FILES['Filedata'.$i]['name'];
+			$uploadfile = $uploaddir.basename($filename);
+			$temploadfile = $_FILES['Filedata'.$i]['tmp_name'];
+			if (!move_uploaded_file($temploadfile , $uploadfile)) {  
+				$isSave = false;
+				break;
+			} 
+		}
+		if($isSave ){
+			echo json_encode("true");
+		}else{
+			echo json_encode("false");
+		}
+	}else{
+		//上传图片名称
+		$filename = $_REQUEST['filename'];
+		if($filename == '')
+		{
+			$filename = $_FILES['Filedata']['name'];
+		}
+		$uploadfile = $uploaddir.basename($filename);
+		$temploadfile = $_FILES['Filedata']['tmp_name'];
+		//move_uploaded_file($temploadfile , $uploadfile);//上传文件
+		if (move_uploaded_file($temploadfile , $uploadfile)) {  
+			echo json_encode("true");
+		} else {
+			echo json_encode("false");
+		} 
+	}
+}else{
+	$flag = unlink($uploaddir.$filename); //删除文件
+	$flag1 = unlink($uploaddir.'small_'.$filename); //删除文件
+}
+
+?>
